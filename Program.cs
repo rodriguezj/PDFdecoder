c5463628b08d04f11ebe8b536017691434905755
rodriguezj <jeremias.j.rodriguez@gmail.com>
2019-10-09 16:16:20 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace ConsoleApp4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                var base64encoding = "JVBERi0xLjQNCiXi48/";
+                byte[] decodePDF = Convert.FromBase64String(base64encoding);
+
+                BinaryWriter writer = new BinaryWriter(File.Open(@"c:\Users\user\desktop\decodedpdf68.pdf", FileMode.CreateNew));
+                writer.Write(decodePDF);
+
+                string decodedString = Encoding.UTF8.GetString(decodePDF);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+                Console.ReadLine();
+            }
+
+
+
+        }
+
+
+    }
+}
